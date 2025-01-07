ff35df795e5ebe05710360ff6888e9754607839d
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-01-07 20:37:25 -0300

    feat: TaskList component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
 import TaskList from "./components/TaskList"
 import AddTask from "./components/AddTask"
+import { useState } from "react"
+
 function App(){
+
+  const [taskList, settaskList] = useState([
+    {
+    id: 1, 
+    title: "Terminar ToDoList",
+    description: "Terminar de fazer esse curso logo e subir todo no github",
+    isCompleted: false
+    },
+    {
+    id: 2, 
+    title: "Terminar ToDoList2",
+    description: "Mais uma vez Terminar de fazer esse curso logo e subir todo no github",
+    isCompleted: false
+    }
+])
+  
+  function onTaskClick(taskListId){
+     const completedTasks = taskList.map(taskList =>{
+
+      //É preciso atualizar a tarefa  
+      if(taskList.id === taskListId){
+        return {...taskList, isCompleted: !taskList.isCompleted}
+      }
+
+      //Não é preciso atualizar a tarefa
+
+      return taskList
+     });
+
+     settaskList(completedTasks);
+  }
+
+  function onClickDeleteTask(taskListId){
+    const maintainedTasks = taskList.filter(taskList => taskList.id !== taskListId)
+    settaskList(maintainedTasks);
+  }
+
   return(
     <div className=" w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas - React
         </h1>
-        <AddTask/>
-        <TaskList/>
+        <TaskList 
+          taskList={taskList} 
+          onTaskClick={onTaskClick} 
+          onClickDeleteTask={onClickDeleteTask}
+        />
       </div>
 
     </div>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,28 @@
-function TaskList(){
-    return(
-        <h1>Suas tarefas</h1>
-    )
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function TaskList({taskList, onTaskClick, onClickDeleteTask}){
+    return (
+        <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+            {taskList.map((taskList) => (
+                <li key={taskList.id} className="flex gap-2">
+                    <button 
+                        onClick={() => onTaskClick(taskList.id)} 
+                        className={`w-full bg-slate-400 text-white p-2 rounded-md 
+                        ${taskList.isCompleted && "line-through"}`}>
+                            {taskList.title}
+                    </button>
+                    <button className="bg-slate-400 p-2 text-white rounded-md">
+                        <ChevronRightIcon/>
+                    </button>
+                    <button 
+                        onClick={() => onClickDeleteTask(taskList.id)}
+                        className="bg-slate-400 p-2 text-white rounded-md">
+                        <TrashIcon/>
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default TaskList
