d2694ab0be3a79f822130fca5f784f1db96fa72c
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-01-08 10:11:17 -0300

    feat: add restriction to blank spaces

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -22,7 +22,19 @@ function AddTask({onClickSubmitButton}){
                 value={description}
                 onChange={(event) => setDescription(event.target.value)}
             />
-            <button onClick={() => onClickSubmitButton(title, description)} className="bg-slate-500 text-white px-4 py-2 rounded-md">Adicionar tarefa</button>
+            <button onClick={() => {
+                //É preciso verificar se o título e a descrição não estão vazios
+
+                if(!title.trim() || !description.trim()){
+                    //Adicionar modal de alerta posteriormente!
+                    return alert("Preecha os espaços em branco!")
+                }
+                onClickSubmitButton(title, description), 
+                setDescription(""),
+                setTitle("")}} 
+                className="bg-slate-500 text-white px-4 py-2 rounded-md">
+                Adicionar tarefa
+            </button>
         </div>
     )
 }
