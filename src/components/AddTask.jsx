5043ae2f0bf124f294f6efa86b3b5ce9a2b7de42
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-01-08 08:50:43 -0300

    feat: created addtask button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,17 @@
 function AddTask(){
     return(
         <div className="flex flex-col bg-slate-200 rounded-md shadow space-y-4 p-6">
-            <input type="text" placeholder="Insira o título da tarefa"/>
-            <input type="text" placeholder="Insira a descrição da tarefa"/>
-
+            <input 
+                className="px-4 py-2 rounded-md border border-slate-300 outline-slate-400" 
+                type="text" 
+                placeholder="Insira o título da tarefa"
+            />
+            <input 
+                className="px-4 py-2 rounded-md border border-slate-300 outline-slate-400" 
+                type="text" 
+                placeholder="Insira a descrição da tarefa"
+            />
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md">Adicionar tarefa</button>
         </div>
     )
 }
