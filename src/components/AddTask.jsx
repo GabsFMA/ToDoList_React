186abafc6405a3cca666a68f6b27d8cb0661b2e4
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-01-08 10:02:04 -0300

    feat: connecting add task function with submit button

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,28 @@
-function AddTask(onClickSubmitButton){
+import { useState } from "react"
+
+function AddTask({onClickSubmitButton}){
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+
     return(
         <div className="flex flex-col bg-slate-200 rounded-md shadow space-y-4 p-6">
             <input 
                 className="px-4 py-2 rounded-md border border-slate-300 outline-slate-400" 
                 type="text" 
                 placeholder="Insira o título da tarefa"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
             />
             <input 
                 className="px-4 py-2 rounded-md border border-slate-300 outline-slate-400" 
                 type="text" 
                 placeholder="Insira a descrição da tarefa"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
             />
-            <button className="bg-slate-500 text-white px-4 py-2 rounded-md">Adicionar tarefa</button>
+            <button onClick={() => onClickSubmitButton(title, description)} className="bg-slate-500 text-white px-4 py-2 rounded-md">Adicionar tarefa</button>
         </div>
     )
 }
