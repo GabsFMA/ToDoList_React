8544c926a343d1ac763c38edbb13a476fbded6ee
Gabriel Francisco <105892017+GabsFMA@users.noreply.github.com>
2025-01-09 14:15:09 -0300

    feat: back button

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,8 +1,10 @@
 import { ChevronLeftIcon } from "lucide-react"
-import { useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 
 function TaskPage(){
 
+
+    const navigate = useNavigate()
     const [SearchParams] = useSearchParams()
     const title = SearchParams.get("title")
     const description = SearchParams.get("description")
@@ -11,7 +13,9 @@ function TaskPage(){
         <div className="w-screen h-screen bg-slate-500 p-6 flex justify-center">
             <div className="w-[500px] space-y-4">
                 <div className="flex justify-center relative m-6">
-                    <button className="absolute left-0 top-0 bottom-0">
+                    <button 
+                        onClick={() => navigate(-1)}
+                        className="absolute left-0 top-0 bottom-0 text-slate-100">
                         <ChevronLeftIcon/>
                     </button>
                     <h1 className="text-3xl text-slate-100 font-bold text-center">
